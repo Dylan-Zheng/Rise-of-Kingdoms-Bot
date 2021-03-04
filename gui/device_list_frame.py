57b98f3e3ee9f20f2e5e233c669ee5d7b4d4378c
Dylan Zheng <dylan20zheng@gmail.com>
2021-03-03 21:22:24 -0500

    add multi-devices control,

---
+++ gui/device_list_frame.py
@@ -0,0 +1,183 @@
+from tkinter import Frame, Label, Entry, Button, LabelFrame
+from tkinter import N, W, LEFT, CENTER
+from gui.selected_device_frame import SelectedDeviceFrame
+from gui.creator import write_device_config, load_device_config
+import adb
+import re
+import traceback
+
+RUNNING = 'running'
+DISCONNECTED = 'disconnected'
+CONNECTED = 'connected'
+
+
+class DeviceListFrame(Frame):
+
+    def __init__(self, notebook, main_frame, cnf={}, **kwargs):
+        Frame.__init__(self, notebook, kwargs)
+        self.windows_size = [kwargs['width'], kwargs['height']]
+
+        self.devices_config = load_device_config()
+
+        self.main_frame = main_frame
+        adf = AddDeviceFrame(self, main_frame)
+        dlt = DeviceListTable(self, main_frame)
+
+        for config in self.devices_config:
+            dlt.add_row(config['ip'], config['port'])
+
+        adf.on_add_click(dlt.add_row)
+        adf.grid(row=0, column=0, pady=(10, 0), sticky=N + W)
+        dlt.grid(row=1, column=0, pady=(10, 0), sticky=N + W)
+
+
+class DeviceListTable(Frame):
+    def __init__(self, parent, main_frame, cnf={}, **kwargs):
+        Frame.__init__(self, parent, kwargs)
+        self.main_frame = main_frame
+        self.title = Label(self, text="Devices:")
+        self.title.grid(row=0, column=0, sticky=W, padx=(5, 0))
+        self.device_rows = []
+
+    def add_row(self, ip, port):
+        try:
+            if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip) is None:
+                return
+            if not (1 <= int(port) <= 65535):
+                return
+
+            new_row = DeviceRow(self, self.main_frame, ip, port)
+            new_row.set_on_display_click(self.on_display_click)
+            new_row.set_on_del_click(self.on_delete_click)
+            self.device_rows.append(new_row)
+            self.render()
+
+        except Exception as e:
+            traceback.print_exc()
+            return
+
+    def remove_row(self, row):
+        try:
+            idx = self.device_rows.index(row)
+            self.device_rows.remove(row)
+            row.device_frame.stop()
+            row.device_frame.destroy()
+            row.destroy()
+        except Exception as e:
+            traceback.print_exc()
+            return
+
+    def on_display_click(self, row):
+        self.master.master.select(1)
+        for device_row in self.device_rows:
+            if device_row.device_frame is not None:
+                device_row.device_frame.grid_forget()
+        row.device_frame.grid()
+
+    def on_delete_click(self, row):
+        ip, port = row.device.serial.split(':')
+        tmp = list(filter(lambda addr: addr['ip'] == ip and addr['port'] == port, self.master.devices_config))
+        self.master.devices_config.remove(
+            next(addr for addr in self.master.devices_config if addr['ip'] == ip and addr['port'] == port)
+        )
+        write_device_config(self.master.devices_config)
+        self.remove_row(row)
+
+    def render(self):
+        for i in range(len(self.device_rows)):
+            self.device_rows[i].grid(row=i + 1, column=0, sticky=W, padx=(10, 0), pady=(10, 0))
+
+
+class DeviceRow(Frame):
+    def __init__(self, device_list_table, main_frame, ip, port, cnf={}, **kwargs):
+        Frame.__init__(self, device_list_table, kwargs)
+
+        self.main_frame = main_frame
+        self.ip = ip
+        self.port = port
+
+        self.device = adb.bridge.get_device(ip, port)
+        self.device_frame = None
+
+        self.name_label = Label(
+            self, text='{}:{}'.format(ip, port), bg='white', height=1, width=20)
+        self.status_label = Label(
+            self, text=DISCONNECTED if self.device is None else CONNECTED, bg='white', width=15
+        )
+
+        self.display_btn = Button(self, text='Display')
+        self.del_btn = Button(self, text='Delete')
+
+        self.name_label.grid(row=0, column=0, sticky=W, padx=(10, 0))
+        self.status_label.grid(row=0, column=1, sticky=W, padx=(10, 0))
+        self.display_btn.grid(row=0, column=2, sticky=W, padx=(10, 0))
+        self.del_btn.grid(row=0, column=3, sticky=W, padx=(10, 0))
+
+    def set_on_del_click(self, on_click=lambda self: self):
+        self.del_btn.config(command=lambda: on_click(self))
+
+    def set_on_display_click(self, on_click=lambda self: self):
+
+        def callback():
+            device = adb.bridge.get_device(self.ip, self.port)
+            if device is None:
+                return
+            if self.device_frame is None:
+                self.status_label.config(text=DISCONNECTED if device is None else CONNECTED)
+                width, height = self.master.master.windows_size
+                self.device_frame = SelectedDeviceFrame(self.main_frame, device, width=width, height=height)
+                self.device_frame.grid(row=0, column=0, sticky=N + W)
+                self.device_frame.grid_forget()
+            on_click(self)
+
+        self.display_btn.config(command=callback)
+
+
+class AddDeviceFrame(Frame):
+
+    def __init__(self, parent, cnf={}, **kwargs):
+        Frame.__init__(self, parent, kwargs)
+
+        self.ip_label = Label(self, text='ip: ')
+        self.ip_entry = Entry(self)
+
+        self.port_label = Label(self, text='port: ')
+        self.port_entry = Entry(self)
+
+        self.add_btn = Button(self, text='Add', width=10)
+
+        def ip_entry_validate_cmd(value, action_type):
+            if action_type == '1':
+                if not value[-1].isdigit() and value[-1] != '.':
+                    return False
+                if value[0] == '0':
+                    return False
+            return True
+
+        def port_entry_validate_cmd(value, action_type):
+            if action_type == '1':
+                if not value[-1].isdigit():
+                    return False
+                if value[0] == '0':
+                    return False
+            return True
+
+        self.ip_entry.config(width=30, validate='key', validatecommand=(
+            self.register(ip_entry_validate_cmd), '%P', '%d'
+        ))
+        self.port_entry.config(width=10, validate='key', validatecommand=(
+            self.register(port_entry_validate_cmd), '%P', '%d'))
+
+        self.ip_label.grid(row=0, column=0, sticky=W, padx=5)
+        self.ip_entry.grid(row=0, column=1, sticky=W, padx=5)
+        self.port_label.grid(row=0, column=2, sticky=W, padx=5)
+        self.port_entry.grid(row=0, column=3, sticky=W, padx=5)
+        self.add_btn.grid(row=0, column=4, sticky=W, padx=5)
+
+    def on_add_click(self, on_click=lambda ip, port: None):
+        def callback():
+            on_click(self.ip_entry.get(), self.port_entry.get())
+            self.master.devices_config.append({'ip': self.ip_entry.get(), 'port': self.port_entry.get()})
+            write_device_config(self.master.devices_config)
+
+        self.add_btn.config(command=callback)
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,18 +1,20 @@
 from tkinter import Tk, N, W
-from tkinter.ttk import Notebook
+from tkinter.ttk import Notebook, Frame
 from gui.setting_frame import SettingFrame
 from gui.bottom_frame import BottomFrame
+from gui.device_list_frame import DeviceListFrame
 from gui.selected_device_frame import SelectedDeviceFrame
 from config import load_config
 from bot_related import twocaptcha, haoi
+import adb
 
 import config
 
 
 class MainWindow:
 
-    def __init__(self, adb, size=(450, 850)):
-        self.adb = adb
+    def __init__(self, size=(450, 850)):
+        self.adb = adb.bridge
         config.global_config = load_config()
         twocaptcha.key = config.global_config.twocaptchaKey
         haoi.userstr = config.global_config.haoiUser
@@ -27,26 +29,30 @@ class MainWindow:
 
         self.curr_frame = None
         self.last_frame = None
-        self.main_frame_list = []
         self.setting_frame = None
 
-        tab = Notebook(self.window, height=size[1] - 80)
+        self.notebook = Notebook(self.window, height=size[1] - 80)
 
-        device = self.adb.get_device()
-        mf = SelectedDeviceFrame(self.window, self.adb.get_device(), width=size[0], height=size[1])
-        self.curr_frame = mf
-        self.main_frame_list.append(mf)
-        mf.grid(row=0, column=0, sticky=N + W)
-        mf.grid_propagate(False)
+        # device = self.adb.get_device('127.0.0.1', 5585)
+        # mf = SelectedDeviceFrame(self.notebook, device, width=size[0], height=size[1])
+        # self.curr_frame = mf
+        # self.main_frame_list.append(mf)
+        # mf.grid(row=0, column=0, sticky=N + W)
+        # mf.grid_propagate(False)
 
-        sf = SettingFrame(self.window, width=size[0], height=size[1])
+        main_frame = Frame(self.notebook, width=size[0], height=size[1])
+
+        dlf = DeviceListFrame(self.notebook, main_frame, width=size[0], height=size[1])
+
+        sf = SettingFrame(self.notebook, width=size[0], height=size[1])
         self.setting_frame = sf
         sf.grid(row=0, column=0, sticky=N + W)
         sf.grid_propagate(False)
 
-        tab.add(mf, text=mf.device.serial)
-        tab.add(sf, text='Setting')
-        tab.grid(row=0, column=0, sticky=N + W, pady=(10, 0))
+        self.notebook.add(dlf, text='Device List')
+        self.notebook.add(main_frame, text='Display Device')
+        self.notebook.add(sf, text='Setting')
+        self.notebook.grid(row=0, column=0, sticky=N + W, pady=(10, 0))
 
         bf = BottomFrame(self.window, width=size[0], height=size[1])
         bf.grid(row=1, column=0, sticky=N + W, padx=10, pady=(10, 0))
