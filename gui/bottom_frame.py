0b831bcd981d337673b7f22af4a3e2e1c9c9df6c
Dylan Zheng <dylan20zheng@gmail.com>
2021-03-14 16:07:42 -0400

    update label when there is new version

--- gui/bottom_frame.py
+++ gui/bottom_frame.py
@@ -1,5 +1,8 @@
 from tkinter import Frame, Label, N, W
 import webbrowser
+from utils import get_last_version_number
+from version import version
+import threading
 
 url = "https://github.com/Dylan-Zheng/Rise-of-Kingdoms-Bot"
 
@@ -10,11 +13,17 @@ class BottomFrame(Frame):
         Frame.__init__(self, windows, kwargs)
         self.windows_size = [kwargs['width'], kwargs['height']]
 
-        label = Label(self, text="Welcome to use Rise of Kingdoms Bot, You can check update on")
+        label = Label(self, text="Welcome to use Rise of Kingdoms Bot, You see update on")
+
+        def callback():
+            last_version = get_last_version_number()
+            if last_version != version:
+                label.config(text='There is new version {}, download at'.format(last_version))
+
+        threading.Thread(target=callback).start()
 
         link = Label(self, text="GitHub", fg="blue", cursor="hand2")
         link.bind("<Button-1>", lambda e: webbrowser.open_new(url))
 
-        label.grid(row=0, column=0, sticky = N + W)
-        link.grid(row=0, column=1, sticky = N + W)
-
+        label.grid(row=0, column=0, sticky=N + W)
+        link.grid(row=0, column=1, sticky=N + W)
